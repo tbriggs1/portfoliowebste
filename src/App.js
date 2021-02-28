86d3d2c838fda3469db08db11e1132577df9139d
tbriggs1 <tombriggs93@protonmail.com>
2021-02-28 15:41:52 +0000

    about component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import AboutSection from './components/AboutSection';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-     
+      <AboutUs />
     </div>
   );
 }
