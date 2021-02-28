3ad4992415003e442dde50695bf7242552064c9a
tbriggs1 <tombriggs93@protonmail.com>
2021-02-28 20:08:38 +0000

    style changes

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {StyledAbout, Description, Hide, Image} from '../styles.js'
+import styled from 'styled-components';
+import {StyledAbout, Description, Hide, Image} from '../styles.js';
+import AboutSection from './AboutSection.js';
 
 
 const FaqSection = () => {
     return(
-        <div className="faq">
+        <FAQ>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How do I start?</h4>
@@ -15,6 +17,7 @@ const FaqSection = () => {
                     </p>
                 </div>
             </div>
+            <div className="faq-line"></div>
             <div className="question">
                 <h4>Daily Schedule?</h4>
                 <div className="answer">
@@ -23,6 +26,7 @@ const FaqSection = () => {
                         Great question! I think that's one of the fundamental and most import things that we look for when developing such components.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
@@ -33,6 +37,7 @@ const FaqSection = () => {
                     </p>
                 </div>
             </div>
+            <div className="faq-line"></div>
             <div className="question">
                 <h4>What products do you offer?</h4>
                 <div className="answer">
@@ -42,8 +47,36 @@ const FaqSection = () => {
                     </p>
                 </div>
             </div>
-        </div>
+            <div className="faq-line"></div>
+        </FAQ>
     )
 }
+
+const FAQ = styled(StyledAbout)`
+display:block;
+span{
+    display:block;
+}
+h2{
+    padding-bottom: 2rem;
+    font-weight: lighter;
+}
+.faq-line{
+    margin: 2rem 0rem;
+    width: 100%;
+    background: #cccccc;
+    height: 0.2rem;
+}
+.question{
+    padding: 3rem 0rem;
+    cursor: pointer;
+}
+.answer{
+    padding: 2rem 0rem;
+    p{
+        padding: 1rem 0rem;
+    }
+}
+`
 
 export default FaqSection;
